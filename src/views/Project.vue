1f79f066e6ddfc4b9d36d97b3a974d10f504614a
joaojesus81 <67010899+joaojesus81@users.noreply.github.com>
2020-12-21 14:30:19 +0000

    single projec page working. reviewed sec nav bar to make sure it references links by name otherwise we keep getting errors

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -9,55 +9,15 @@
 </template>
 
 <script>
-import { reactive } from "vue";
 import { useRoute } from "vue-router";
-import getProjects from "@/composables/getProjects.js";
+import getSingleProject from "@/composables/getSingleProject.js";
 
 export default {
-  // props: ["projects"],
   setup() {
-    const { projects, loadProjects } = getProjects();
-
     const route = useRoute();
-    console.log("route params", route.params);
 
-    let project = reactive({});
-
-    for (let i; i < projects.length; i++) {
-      console.log("iiiiii: ", i);
-    }
-
-    const singleProject = () => {
-      console.log("temp: ", tempProject);
-      // console.log("Params: ", $route.params.project_number)
-      // const tempProject = props.projects.filter(
-      //   (proj) => {
-      //     console.log(proj.project_number, props.project_number, proj.project_number === props.project_number)
-      //     return proj.project_number === props.project_number}
-      // );
-      const tempProject = () => {
-        console.log("start of temp");
-        // console.log("start of temp projects", props.projects)
-        for (let i; i < projects.length; i++) {
-          console.log(
-            "inside temp, project number: ",
-            route.params.project_number
-          );
-          if (projects[i].project_number == route.params.project_number) {
-            console.log("inside temp, proj: ", projects[i]);
-            return projects[i];
-          } else {
-            console.log("fail");
-          }
-        }
-      };
-      console.log("outside temp, tempProject: ", tempProject);
-      project = tempProject();
-      console.log("outside temp, project: ", project);
-    };
-    loadProjects();
-    console.log(projects);
-    singleProject();
+    const { project, loadSingleProject } = getSingleProject();
+    loadSingleProject(route.params.project_number);
 
     return { project };
   },
