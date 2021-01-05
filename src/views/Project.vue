78d1c7967ac4c3618786c580d8b2668c111dcad6
jcvsilva <jcvsilva@gmail.com>
2021-01-05 15:48:53 +0000

    Fix for small bug, removed route and emit from project.

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -15,18 +15,17 @@
 import getSingleProject from "@/composables/getSingleProject";
 import Spinner from "@/components/Spinner";
 import RecordIssues from "@/components/RecordIssues";
-import { useRoute } from 'vue-router';
 import ProjectComponent from "@/components/ProjectComponent";
 
 export default {
   props: ["stageselect"],
   emits: ["reload"],
   components: { Spinner, RecordIssues, ProjectComponent },
-  setup(_, {emit}) {
+  setup() {
+
     // this section is for fetching a single project
     const { project, loadSingleProject } = getSingleProject();
     loadSingleProject();
-    const route = useRoute();
 
     return { project, loadSingleProject };
   },
